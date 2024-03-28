0ffee7f5f599b1375ec64450837fefd11d2d885a
Andrew Stiefel <andrew@andrewstiefel.com>
2024-03-28 16:43:02 -0700

    Create event triggered function example

---
+++ netlify/functions/submission-created.js
@@ -0,0 +1,35 @@
+import { createClient } from "@1password/sdk";
+import fetch from 'node-fetch';
+
+const { OP_SERVICE_ACCOUNT_TOKEN } = process.env;
+
+export default async (event) => {
+    const email = JSON.parse(event.body).payload.email
+    console.log(`Received a submission: ${email}`)
+
+    const client = await createClient({
+        auth: OP_SERVICE_ACCOUNT_TOKEN,
+        integrationName: "Netlify and 1Password Demo",
+        integrationVersion: "v1.0.0",
+      });
+      
+    const secret = await client.secrets.resolve("op://sdk-beta/hello-world/credential");
+    console.log(secret)
+
+    const response = await fetch( 'https://api.buttondown.email/v1/subscribers', {
+        method: 'POST',
+        headers: {
+            Authorization: `Token ${BUTTONDOWN_API_KEY}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+    });
+
+    let responseText = await response.text();
+    console.log('Response:', responseText);
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify({})
+    }
+};
