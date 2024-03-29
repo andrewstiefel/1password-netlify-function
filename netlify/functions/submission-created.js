1491b22f879a31894315297f3184da7f05e8cf97
Andrew Stiefel <andrew@andrewstiefel.com>
2024-03-29 02:39:21 +0000

    Update function

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,6 +1,9 @@
 export default async (request, context) => {
-  const email = JSON.parse(request).payload.email
-  console.log(email)
+  const body = await request.json();
+  console.log(body)
+
+  const obj = body.payload.email
+  console.log(obj)
     
   return new Response()
 };
